3b16403ab95cb3aadfd22a7126ec0913237d30fe
Elyes T. <139150024+CodeSailor411@users.noreply.github.com>
2024-08-09 11:26:14 +0100

    Update vqa_finetune.py

--- VQA/vqa_finetune.py
+++ VQA/vqa_finetune.py
@@ -38,7 +38,7 @@ trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=tokenized_dataset['train'],
-    eval_dataset=tokenized_dataset['validation']
+    eval_dataset=tokenized_dataset['test']
 )
 
 # Fine-tune the model
